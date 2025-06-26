f5dc16ce9b6835fc048e3cd62d5f4b7d8dc54c31
Palex-l <palex.l@gmx.net>
2025-06-26 20:02:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,6 @@ $datei = "/tmp/daten.json";
 $daten = [];
 
 if (file_exists($datei)) {
-    $jsonInhalt = file_get_contents($datei);
-    $daten = json_decode($jsonInhalt, true);
-    if (!is_array($daten)) {
-        $daten = [];
-    }
-}
-?>
-
-<?php
-$datei = "/tmp/daten.json";
-$daten = [];
-
-if (file_exists($datei)) {
     $json = file_get_contents($datei);
     $daten = json_decode($json, true);
     if (!is_array($daten)) {
