ce71598bbb731bb884804f8a5b3d1d55cdcbc160
Palex-l <palex.l@gmx.net>
2025-06-26 16:51:40 +0200

    Update daten.php

--- daten.php
+++ daten.php
@@ -1,30 +1,16 @@
 <?php
-$dataFile = "daten.json";
+header('Content-Type: application/json');
+
+$datei = "/tmp/daten.json";
 $daten = [];
 
-if (file_exists($dataFile)) {
-    $json = file_get_contents($dataFile);
+if (file_exists($datei)) {
+    $json = file_get_contents($datei);
     $daten = json_decode($json, true);
-}
-
-$tabelle = $daten;
-$radar = [];
-$winkelSet = [];
-
-for ($i = count($daten) - 1; $i >= 0; $i--) {
-    $e = $daten[$i];
-    $winkel = $e['sensor1'];
-    if (!isset($winkelSet[$winkel])) {
-        $radar[] = ['winkel' => $winkel, 'dist' => $e['sensor2']];
-        $winkelSet[$winkel] = true;
+    if (!is_array($daten)) {
+        $daten = [];
     }
-    if (count($radar) >= 20) break;
 }
 
-$radar = array_reverse($radar);
-
-header('Content-Type: application/json');
-echo json_encode([
-    'tabelle' => $tabelle,
-    'radar' => $radar
-]);
+echo json_encode($daten);
+?>
