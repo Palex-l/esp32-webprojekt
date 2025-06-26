8fc9b7d7d6eb96451d7b895ffc7401294b0b0e33
Palex-l <palex.l@gmx.net>
2025-06-26 16:38:51 +0200

    Update daten.php

--- daten.php
+++ daten.php
@@ -1,10 +1,30 @@
 <?php
-$datei = "/tmp/daten.json"; // oder daten.txt falls du das verwendest, dann parse entsprechend
+$dataFile = "daten.json";
+$daten = [];
 
-if (file_exists($datei)) {
-    header('Content-Type: application/json');
-    echo file_get_contents($datei);
-} else {
-    echo json_encode([]);
+if (file_exists($dataFile)) {
+    $json = file_get_contents($dataFile);
+    $daten = json_decode($json, true);
 }
-?>
+
+$tabelle = $daten;
+$radar = [];
+$winkelSet = [];
+
+for ($i = count($daten) - 1; $i >= 0; $i--) {
+    $e = $daten[$i];
+    $winkel = $e['sensor1'];
+    if (!isset($winkelSet[$winkel])) {
+        $radar[] = ['winkel' => $winkel, 'dist' => $e['sensor2']];
+        $winkelSet[$winkel] = true;
+    }
+    if (count($radar) >= 20) break;
+}
+
+$radar = array_reverse($radar);
+
+header('Content-Type: application/json');
+echo json_encode([
+    'tabelle' => $tabelle,
+    'radar' => $radar
+]);
